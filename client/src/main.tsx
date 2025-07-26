a6f7d5f90241c109cfdc15cc39ded0ef0a60ee22
Shray Arora <116770579+shrayarora99@users.noreply.github.com>
2025-07-26 00:28:41 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById('root');
+if (container) {
+  createRoot(container).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
